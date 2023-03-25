3efb8386756ee1effb649be068a5bfa9e79dcabb
Ojidaemo <vit.martsinovich@gmail.com>
2023-03-25 20:29:04 +0200

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Vitali Martsinovich on 2023-03-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
